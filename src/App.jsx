96410a28ac85f9052d73607380c5c3aede62b420
Cristian Contreras <contreras.cristian994@gmail.com>
2025-12-29 03:10:09 -0500

    Sidebar updated, overview page added

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
       <div className='fixed inset-0 z-0'>
         <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80'></div>
-        {/* <div className='absolute inset-0 backdrop-blur-sm'></div> */}
+        <div className='absolute inset-0 backdrop-blur-sm'></div>
       </div>
       <SideBar />
       <Routes>
