0d6105fc07c3ed7980e1434d9dec8f301d824b5d
Cristian Contreras <contreras.cristian994@gmail.com>
2025-12-30 01:07:56 -0500

    sales chart added

---
+++ src/components/overview/SalesOverviewChart.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+import { animate, motion } from 'framer-motion'
+
+const salesData = [
+  { name: "Jul", sales: 7000 },
+  { name: "Aug", sales: 8000 },
+  { name: "Sep", sales: 9000 },
+  { name: "Oct", sales: 10000 },
+  { name: "Nov", sales: 11000 },
+  { name: "Dec", sales: 12000 },
+  { name: "Jan", sales: 1000 },
+  { name: "Feb", sales: 2000 },
+  { name: "Mar", sales: 3000 },
+  { name: "Apr", sales: 4000 },
+  { name: "May", sales: 5000 },
+  { name: "Jun", sales: 6000 },
+];
+
+const SalesOverviewChart = () => {
+  return (
+    <motion.div
+      className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700'
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+    >
+      <h2>Sales Overview</h2>
+      <div className='h-80'>
+        <ResponsiveContainer width={"100%"} height={"100%"}>
+          <LineChart data={salesData}>
+            <CartesianGrid strokeDasharray='3 3' stroke='#4B5563' />
+            <XAxis dataKey={"name"} stroke='#9CA3AF'/>
+            <YAxis stroke='#9CA3AF' />
+            <Tooltip 
+              contentStyle={{
+                backgroundColor: "rgba(31, 41, 55, 0.8)",
+                borderColor: "#4B5563"
+              }}   
+              itemStyle={{ color: "#E5E7EB" }}
+            />
+            <Line 
+              type="monotone"
+              dataKey="sales"
+              stroke='#6366F1'
+              strokeWidth={3}
+              dot={{ fill: "#6366F1", strokeWidth: 2, r: 6 }}
+              activeDot={{ r: 8, strokeWidth: 2 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </motion.div>
+  )
+}
+
+export default SalesOverviewChart
